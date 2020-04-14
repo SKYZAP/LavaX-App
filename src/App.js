5328d45cbe43b1f97730f9ce3369cdee145e246e
SKYZAP <izzat.jehan@gmail.com>
2020-04-14 19:34:50 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
- 
-const Marker = ({ text }) => <div>{text}</div>;
- 
+  
 class CovidMap extends Component {
   constructor(props){
     super(props);
     this.state ={
       Covid: [],
-      country: '',
-      confirmed: '',
-      recovered: '',
-      critical: '',
-      deaths: '',
-      latitude: '',
-      longitude: '',
       isLoading: false,
+      Totals: [],
     }
   }
 
@@ -36,6 +28,24 @@ class CovidMap extends Component {
     });
   }
 
+  fetchTotals(){
+    fetch("https://covid-19-data.p.rapidapi.com/totals?format=undefined", {
+	    "method": "GET",
+	    "headers": {
+		    "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+		    "x-rapidapi-key": "34bd11e989msh39e775948fdba82p1d191ajsna92920e8f23c"
+	    }
+    })
+    .then(response => response.json().then((result)=>{
+      this.setState({Totals: result});
+      console.log(this.state.Totals);
+    }))
+    .catch(err => {
+	    console.log(err);
+    });
+     console.log(this.state.Totals);
+  }
+
   getCountryDatabyName(name){
     const encodedURL = encodeURIComponent(name);
     fetch("https://covid-19-data.p.rapidapi.com/country?format=undefined&name="+encodedURL, {
@@ -51,6 +61,7 @@ class CovidMap extends Component {
     .catch(err => {
 	    console.log(err);
     });
+ 
   }
 
   showData(){
@@ -66,13 +77,31 @@ class CovidMap extends Component {
     },
     zoom: 11
   };
+
  
   render() {
-
-    if (this.state.Covid){
+    const countrylocation = this.state.Covid.map((data,i)=>{
+      return(
+        <div lat={data.latitude}
+        lng={data.longitude}
+        style={{
+          color: "red",
+          backgroundColor: "#FFF",
+          height: "25px",
+          width: "35px",
+          textAlign: "center",
+          borderRadius: "30%",
+        }}>
+          Total <br></br>
+        {data.confirmed}
+        </div>
+      )
+    });
+      
+    if (this.state.Covid && this.state.Totals){
       const listItems = this.state.Covid.map((item,index)=>
       <div key={index} style={{float: 'left', padding: '20px', borderStyle: 'solid', textAlign: 'center'}}>
-        <div style={{width: '200px', height: '50px'}}>
+        <div style={{width: '190px', height: '50px'}}>
             <h3>{item.country}</h3>
         </div>
         <div>Confirmed: {item.confirmed} </div>
@@ -80,22 +109,53 @@ class CovidMap extends Component {
         <div>Critical: {item.critical} </div>
         <div>Deaths: {item.deaths} </div>
       </div>);
+      
+      const listTotal = this.state.Totals.map((item,index)=>
+      <div>
+        <div style={{
+          padding: "10px",
+          backgroundColor: "grey",
+        }}>Confirmed: {item.confirmed}</div>
+        <div style={{
+          padding: "10px",
+          backgroundColor: "green",
+        }}>Recovered: {item.recovered}</div>
+        <div style={{
+          padding: "10px",
+          backgroundColor: "red",
+        }}>Critical: {item.critical}</div>
+        <div style={{
+          borderStyle:"solid",
+        }}>Deaths: {item.deaths}</div>
+      </div>
+      );
     return (
       <div>
-        <div style={{ height: '50vh', width: '50vw' }}>
+        <h1 style={{
+          height: "50px",
+          width: "100%",
+          textAlign: "center",
+          padding: "20px",
+          margin: "0px",
+          borderColor: "black",
+          borderStyle: "solid",
+        }}>
+          Covid 19 Stats
+        </h1>
+        <div style={{ height: '70vh', width: '50vw', borderstyle:"solid" , float:"left"}}>
           <GoogleMapReact
             bootstrapURLKeys={{ key: 'AIzaSyCz_OE36IuPr4x31shAoamjs1sFzOMqyvU' }}
-            defaultCenter={this.props.center}
-            defaultZoom={this.props.zoom}
+            defaultCenter={{lat: 3.519863 , lng: 101.538116}}
+            defaultZoom={5}
           >
-            <Marker
-              lat={59.955413}
-              lng={30.337844}
-              text="M"
-            />
+           {countrylocation}
           </GoogleMapReact>
         </div>
-        <div>
+        <div className="TotalInfoWrapper" style={{width:"49vw", height: "200px", float:"left", textAlign:"center", margin: "0px", padding: "0px"}}>
+            <div style={{fontSize:"30px"}}>Total : {listTotal}</div>
+            <button onClick={()=>this.fetchTotals()}>Update Totals</button>
+        </div>  
+        <div style={{overflow: "hidden", float:"left"}}>
           {listItems}
         </div>
       </div>
